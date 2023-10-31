20e8f27acaaf6c7d14af3728f03ec1ebbecc23ec
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-10-31 15:50:37 +0600

    added loader with tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,19 @@ import {
 } from "react-router-dom";
 import router from './Routes/Routes.jsx';
 import AuthProvider from './providers/AuthProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='max-w-7xl mx-auto'>
     <React.StrictMode>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </QueryClientProvider>
+
     </React.StrictMode>,
   </div>
 )
--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,63 +1,85 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import BookingRow from "./BookingRow";
+import axios, { Axios } from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 const Bookings = () => {
     const { user } = useContext(AuthContext);
-    const [bookings, setBookings] = useState([]);
+    // const [bookings, setBookings] = useState([]);
+    const url = `http://localhost:3000/booking?email=${user?.email}`;
 
-    const url = `http://localhost:3000/bookings?email=${user?.email}`;
+    // console.log(bookings)
+
+
+    const { data: bookings ,isPending,refetch } = useQuery({
+        queryKey: ['userBookings'],
+        queryFn: async () => {
+            const data =await fetch(url)
+            return await data.json();
+        }
+    })
+
+    console.log( isPending)
 
     
-    useEffect(() => {
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setBookings(data))
-    }, [url]);
+
+    // useEffect(() => {
+    //     fetch(url)
+    //         .then(res => res.json())
+    //         .then(data => setBookings(data))
+    // }, [url]);
+
+
+
+
 
     const handleDelete = id => {
         const proceed = confirm('Are You sure you want to delete');
         if (proceed) {
-            fetch(`http://localhost:5000/bookings/${id}`, {
-                method: 'DELETE'
-            })
-                .then(res => res.json())
+
+            axios.delete(`http://localhost:3000/booking/${id}`)
                 .then(data => {
-                    console.log(data);
-                    if (data.deletedCount > 0) {
+                    if (data.data.deletedCount > 0) {
+                        refetch()
                         alert('deleted successful');
-                        const remaining = bookings.filter(booking => booking._id !== id);
-                        setBookings(remaining);
+                        // const remaining = bookings.filter(bookingSingle => bookingSingle._id !== id);
+                        // setBookings(remaining);
                     }
                 })
         }
     }
 
     const handleBookingConfirm = id => {
-        fetch(`http://localhost:5000/bookings/${id}`, {
-            method: 'PATCH',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify({ status: 'confirm' })
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                if (data.modifiedCount > 0) {
-                    // update state
-                    const remaining = bookings.filter(booking => booking._id !== id);
-                    const updated = bookings.find(booking => booking._id === id);
-                    updated.status = 'confirm'
-                    const newBookings = [updated, ...remaining];
-                    setBookings(newBookings);
-                }
-            })
+        // fetch(`http://localhost:5000/bookings/${id}`, {
+        //     method: 'PATCH',
+        //     headers: {
+        //         'content-type': 'application/json'
+        //     },
+        //     body: JSON.stringify({ status: 'confirm' })
+        // })
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         console.log(data);
+        //         if (data.modifiedCount > 0) {
+        //             // update state
+        //             const remaining = bookings.filter(booking => booking._id !== id);
+        //             const updated = bookings.find(booking => booking._id === id);
+        //             updated.status = 'confirm'
+        //             const newBookings = [updated, ...remaining];
+        //             setBookings(newBookings);
+        //         }
+        //     })
+    }
+
+
+    if(isPending){
+        return <>hello</>
     }
 
     return (
         <div>
-            <h2 className="text-5xl">Your bookings: {bookings.length}</h2>
+            <h2 className="text-5xl">Your bookings: {bookings?.length}</h2>
             <div className="overflow-x-auto w-full">
                 <table className="table w-full">
                     {/* head */}
@@ -77,7 +99,7 @@ const Bookings = () => {
                     </thead>
                     <tbody>
                         {
-                            bookings.map(booking => <BookingRow
+                            bookings?.map(booking => <BookingRow
                                 key={booking._id}
                                 booking={booking}
                                 handleDelete={handleDelete}
